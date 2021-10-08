44fe7c3af6529e90007cd19663a487ae4fbbf6a2
Akash Ramanand Rajak <aakashrajak02@gmail.com>
2021-10-08 09:31:17 +0530

    Updated persondetection.py

--- persondetection.py
+++ persondetection.py
@@ -1,8 +1,5 @@
 
-# from random import randint
-# from collections import OrderedDict
-# from scipy.spatial import distance as dist
-# import cv2 as cv
+
 import numpy as np
 import tensorflow as tf
 # import cv2
@@ -11,78 +8,6 @@ import os
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
-# class TrackableObject:
-#     def __init__(self, objectID, centroid):
-#         self.objectID = objectID
-#         self.centroids = [centroid]
-#         self.counted = False
-#
-# class CentroidTracker:
-#     def __init__(self, maxDisappeared=50, maxDistance=50):
-#         self.nextObjectID = 0
-#         self.objects = OrderedDict()
-#         self.disappeared = OrderedDict()
-#         self.maxDisappeared = maxDisappeared
-#         self.maxDistance = maxDistance
-#
-#     def register(self, centroid):
-#         self.objects[self.nextObjectID] = centroid
-#         self.disappeared[self.nextObjectID] = 0
-#         self.nextObjectID += 1
-#
-#     def deregister(self, objectID):
-#         del self.objects[objectID]
-#         del self.disappeared[objectID]
-#
-#     def update(self, rects):
-#         if len(rects) == 0:
-#             for objectID in list(self.disappeared.keys()):
-#                 self.disappeared[objectID] += 1
-#                 if self.disappeared[objectID] > self.maxDisappeared:
-#                     self.deregister(objectID)
-#             return self.objects
-#
-#         inputCentroids = np.zeros((len(rects), 2), dtype="int")
-#
-#         for (i, (startX, startY, endX, endY)) in enumerate(rects):
-#             cX = int((startX + endX) / 2.0)
-#             cY = int((startY + endY) / 2.0)
-#             inputCentroids[i] = (cX, cY)
-#         if len(self.objects) == 0:
-#             for i in range(0, len(inputCentroids)):
-#                 self.register(inputCentroids[i])
-#         else:
-#             objectIDs = list(self.objects.keys())
-#             objectCentroids = list(self.objects.values())
-#             D = dist.cdist(np.array(objectCentroids), inputCentroids)
-#             rows = D.min(axis=1).argsort()
-#             cols = D.argmin(axis=1)[rows]
-#             usedRows = set()
-#             usedCols = set()
-#
-#             for (row, col) in zip(rows, cols):
-#                 if row in usedRows or col in usedCols:
-#                     continue
-#                 if D[row, col] > self.maxDistance:
-#                     continue
-#                 objectID = objectIDs[row]
-#                 self.objects[objectID] = inputCentroids[col]
-#                 self.disappeared[objectID] = 0
-#                 usedRows.add(row)
-#                 usedCols.add(col)
-#
-#             unusedRows = set(range(0, D.shape[0])).difference(usedRows)
-#             unusedCols = set(range(0, D.shape[1])).difference(usedCols)
-#             if D.shape[0] >= D.shape[1]:
-#                 for row in unusedRows:
-#                     objectID = objectIDs[row]
-#                     self.disappeared[objectID] += 1
-#                     if self.disappeared[objectID] > self.maxDisappeared:
-#                         self.deregister(objectID)
-#             else:
-#                 for col in unusedCols:
-#                     self.register(inputCentroids[col])
-#         return self.objects
 
 class DetectorAPI:
     def __init__(self):
